f051bd4f3daa324e9cc7c3c99dfc85c324ae9b82
jb2438 <jb2438@cornell.edu>
2024-03-17 11:29:34 -0400

    Deleted the changes

--- backend/app.py
+++ backend/app.py
@@ -17,10 +17,9 @@ json_file_path = os.path.join(current_directory, 'init.json')
 
 # Assuming your JSON data is stored in a file named 'init.json'
 with open(json_file_path, 'r') as file:
-    d = json.load(file)
-    data = pd.DataFrame.from_dict(d)
-    # episodes_df = pd.DataFrame(data['episodes'])
-    # reviews_df = pd.DataFrame(data['reviews'])
+    data = json.load(file)
+    episodes_df = pd.DataFrame(data['episodes'])
+    reviews_df = pd.DataFrame(data['reviews'])
 
 app = Flask(__name__)
 CORS(app)
@@ -28,14 +27,9 @@ CORS(app)
 # Sample search using json with pandas
 def json_search(query):
     matches = []
-    # merged_df = pd.merge(episodes_df, reviews_df, left_on='id', right_on='id', how='inner')
-    # matches = merged_df[merged_df['title'].str.lower().str.contains(query.lower())]
-    # matches_filtered = matches[['title', 'descr', 'imdb_rating']]
-    # matches_filtered_json = matches_filtered.to_json(orient='records')
-
-    # Replace this with our search method
-    matches = data[data["Title"].str.lower().str.contains(query.lower())]
-    matches_filtered = matches[['Title', 'Author', 'text']]
+    merged_df = pd.merge(episodes_df, reviews_df, left_on='id', right_on='id', how='inner')
+    matches = merged_df[merged_df['title'].str.lower().str.contains(query.lower())]
+    matches_filtered = matches[['title', 'descr', 'imdb_rating']]
     matches_filtered_json = matches_filtered.to_json(orient='records')
     return matches_filtered_json
 
@@ -45,7 +39,7 @@ def home():
 
 @app.route("/episodes")
 def episodes_search():
-    text = request.args.get("Title")
+    text = request.args.get("title")
     return json_search(text)
 
 if 'DB_NAME' not in os.environ:
