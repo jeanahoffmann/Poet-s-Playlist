46e501c040ef33df3b7104ce6689792674bc2112
jb2438 <jb2438@cornell.edu>
2024-03-25 14:27:55 -0400

    Updated base.html and app.py to display output

--- backend/app.py
+++ backend/app.py
@@ -43,8 +43,10 @@ def json_search(query):
     # Assuming query is a string poem title that exists in the database...
         # returns a sorted list of song indices (i.e. most_similar_songs[0] is the idx of the most similar song)
         # OR returns an empty list if the poem title doesn't match a poem in the database
+    
     most_similar_songs = whole_shebang(query)
     top_indexes = most_similar_songs[:10]
+    # top_indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     with open('init.json', 'r', encoding='utf-8') as f: # CHANGE: Changed the json file the code reads
         songs_data = json.load(f)
@@ -57,7 +59,7 @@ def json_search(query):
     # top_titles = [song['song_name'] for song in songs_data if song['song_id'] in top_indexes]
     
     # Copied from the old code above
-    matches_filtered_json = top_titles.to_json() # TODO: Is this the correct format?
+    matches_filtered_json = top_titles.to_json(orient='records') # TODO: Is this the correct format?
     return (matches_filtered_json)
     
     
