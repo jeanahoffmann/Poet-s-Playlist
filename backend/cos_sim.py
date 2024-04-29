610b0830febb105cbae419e5b205598ec5908fc2
Jiye100 <22jiye.baek@gmail.com>
2024-04-29 09:38:39 -0400

    Add get_top_contributing_terms

--- backend/cos_sim.py
+++ backend/cos_sim.py
@@ -115,6 +115,27 @@ def whole_shebang(query, genre): # 4/14 - Treating query as a list of poem names
 
   return(cos_pop_sorted_list_of_docs) # change input to sorted_list_of_docs 
 
+def get_top_contributing_terms(query):
+    songs_and_poems, idf, inv_idx = load_data()
+    poem_dataset = songs_and_poems[0]['poems']
+    poem_indices = get_poem_indices(query, poem_dataset)
+    word_counts = get_all_word_counts(poem_indices, poem_dataset)
+
+    # Calculate contributions
+    term_contributions = get_contributions(word_counts, inv_idx, idf)
+
+    # Format and return top contributing terms
+    top_contributing_terms = {}
+    for doc_id, terms in term_contributions.items():
+        # Sum contributions for each term across documents and sort them
+        for term, contribution in terms.items():
+            if term not in top_contributing_terms:
+                top_contributing_terms[term] = 0
+            top_contributing_terms[term] += contribution
+        sorted_terms = sorted(top_contributing_terms.items(), key=lambda item: item[1], reverse=True)
+
+    return dict(sorted_terms[:10])  # Return top 10 terms - can be changed
+
 
 
 def rocchio_update(song_dataset, relevant, irrelevant):
